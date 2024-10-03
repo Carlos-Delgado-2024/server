2638500bd83e808c3a8cd44574bfe8d1ab364609
Carlos Delgado <megaloto.2024@gmail.com>
2024-10-03 18:21:39 -0500

    despliegue 3

--- config/firebase.js
+++ config/firebase.js
@@ -1,5 +1,6 @@
 const admin = require('firebase-admin');
-const serviceAccount = JSON.parse(process.env.FIREBASE_ADMIN_SDK_JSON);
+const serviceAccount = JSON.parse(process.env.FIREBASE_ADMIN_SDK_JSON)
+console.log(serviceAccount)
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
