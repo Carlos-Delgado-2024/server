88c5926bf5c03b3801af8985bf143a9dddc7a9b6
Carlos Delgado <megaloto.2024@gmail.com>
2024-10-03 18:53:09 -0500

    despligue prueba full 1

--- serverFull.js
+++ serverFull.js
@@ -1,3 +1,39 @@
-const port = process.env.FIREBASE_ADMIN_SDK_JSON
+const http = require('http');
+const { Server } = require('socket.io');
+const {BuscarCollection, listenChanges} = require('./functions/collection')
+const PORT = process.env.PORT || 5000
 
-console.log('este es el firebase: ',port)
+// Crear el servidor HTTP
+const server = http.createServer();
+
+// Configuración de CORS para permitir solo ciertas URL
+const io = new Server(server, {
+  cors: {
+    origin: ['https://megaloto-vfinal.web.app', 'http://localhost:3000'],  // URLs permitidas
+    methods: ['GET', 'POST'],  // Métodos permitidos
+  }
+});
+
+// Manejar la conexión con Socket.IO
+io.on('connection', async(socket) => {
+    console.log('Un usuario se ha conectado');
+    //datos de empresa
+    const empresa = await BuscarCollection('empresa')
+    socket.emit('CollectionEmpresa', empresa);  // Enviar el número de vuelta al cliente
+    listenChanges('empresa',io)
+    //datos de sorteo
+    const sorteos = await BuscarCollection('sorteos')
+    socket.emit('CollectionSorteos', sorteos)
+    listenChanges('sorteos',io)
+
+
+
+  socket.on('disconnect', () => {
+    console.log('Usuario desconectado');
+    });
+});
+
+// Iniciar el servidor en el puerto 3000
+server.listen(PORT, () => {
+  console.log('Servidor escuchando en el puerto: ',PORT);
+});
