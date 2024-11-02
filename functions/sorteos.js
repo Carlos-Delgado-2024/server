fb27ac80550e38eab88963c639a98e5053c6a762
Carlos Delgado <megaloto.2024@gmail.com>
2024-11-02 02:21:26 -0500

    update 2.0.5 sorteoExpress 6

--- functions/sorteos.js
+++ functions/sorteos.js
@@ -67,7 +67,7 @@ const comprarNumeros = async (data) => {
       });
       // console.log(datadoc.typeLot)
       if(datadoc.typeLot==='Express'){
-        const acumulado = (datadoc.premioBase + (datadoc.valor*0.7)) * data.seleccionados.length
+        const acumulado = (datadoc.premioBase + (datadoc.valor*0.7) * data.seleccionados.length)
         await db.collection('sorteos').doc(data.id).update({
           'premioBase':acumulado
         })
