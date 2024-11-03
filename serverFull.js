a56eb2443186427e5a1e9c7b7889aafb3c8e05d1
Carlos Delgado <megaloto.2024@gmail.com>
2024-11-03 14:34:29 -0500

    update 2.0.6 reset express 4

--- serverFull.js
+++ serverFull.js
@@ -35,7 +35,7 @@ io.on('connection', async(socket) => {
   // const ahora = new Date()
   // console.log(ahora)
 ///sorteo express/////
-  cron.schedule('24 * * * *', () => {
+  cron.schedule('37 * * * *', () => {
     console.log('se inicio reset')
     resetExpress()
   })
