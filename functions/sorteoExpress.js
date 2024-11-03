904d003fcdd56b0e624d61efac7939a5c1d2ebb8
Carlos Delgado <megaloto.2024@gmail.com>
2024-11-03 14:32:48 -0500

    update 2.0.6 reset express 3

--- functions/sorteoExpress.js
+++ functions/sorteoExpress.js
@@ -2,31 +2,29 @@ const { admin } = require('../config/firebase')
 const db = admin.firestore()
 
 const resetExpress = async()=>{
-    const result = []
     try{
         const snapshot = await db.collection('sorteos').get()
-        snapshot.forEach(doc => {
+        snapshot.forEach(async(doc) => {
             const data = doc.data()
-            console.log('esto es data', data)
+            // console.log('esto es data', data)
             if (data.typeLot === 'Express'){
-                result.push(data)
+                const ArrayPuestos = []
+                for(let i = 0; i <= data.puestos-1;i++){
+                    const numero = i.toString().padStart(data.puestos.toString().length-1,'0')
+                    ArrayPuestos.push({[numero]:''})
+                }
+                await db.collection('sorteos').doc(doc.id).update({
+                    'arryPuestos': ArrayPuestos,
+                    'ganador':''
+                })
+
             }
         })
+
     }catch (error) {
         console.error('Error al obtener documentos:', error);
         throw error; // Manejo de errores
     }
-    console.log(result)
-    // result.map(async(sorteo)=>{
-    //     const ArrayPuestos = []
-    //     for(let i = 0; i <= sorteo.puestos-1;i++){
-    //         const numero = i.toString().padStart(sorteo.puestos.toString().length-1,'0')
-    //         ArrayPuestos.push({[numero]:''})
-    //     }
-    //     await db.collection('sorteos').doc(sorteo.id).update({
-    //         'arryPuestos': ArrayPuestos,
-    //         'ganador':''
-    //     })
-    // })
+    
 }
 module.exports = {resetExpress}
